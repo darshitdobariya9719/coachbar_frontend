ed0bb922e2de597554651da888df84c49620151c
darshit.dobariya@cybercom.in <darshitdobariya9719@gmail.com>
2025-03-19 20:56:00 +0530

    add login issue

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { useDispatch } from "react-redux";
 import { setUser } from "../redux/authSlice";
-import { Box, TextField, Button, Container, Typography } from "@mui/material";
+import { Box, TextField, Button, Container, Typography, InputAdornment, IconButton } from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 import api from "../utils/api";
 
 const schema = yup.object({
@@ -14,6 +16,11 @@ const schema = yup.object({
 export default function Login() {
   const { register, handleSubmit, formState: { errors } } = useForm({ resolver: yupResolver(schema) });
   const dispatch = useDispatch();
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
 
   const onSubmit = async (data) => {
     try {
@@ -22,7 +29,7 @@ export default function Login() {
       // window.location.href = "/dashboard";
     } catch (err) {
       console.error(err);
-      alert(error.response?.data?.message || "Login failed");
+      alert(err.response?.data?.message || "Login failed");
     }
   };
 
@@ -33,9 +40,38 @@ export default function Login() {
       </Box>
       <form onSubmit={handleSubmit(onSubmit)}>
         <Box sx={{ display: "flex", flexDirection: "column", gap: "10px", alignItems: "end" }}>
-          <TextField fullWidth label="Email" {...register("email")} error={!!errors.email} helperText={errors.email?.message} />
-          <TextField fullWidth label="Password" type="password" {...register("password")} error={!!errors.password} helperText={errors.password?.message} />
-          <Button type="submit" variant="contained" color="primary" sx={{ width: "30px" }}>Login</Button>
+          {/* Email Field */}
+          <TextField
+            fullWidth
+            label="Email"
+            {...register("email")}
+            error={!!errors.email}
+            helperText={errors.email?.message}
+          />
+
+          {/* Password Field with Show/Hide Toggle */}
+          <TextField
+            fullWidth
+            label="Password"
+            type={showPassword ? "text" : "password"}
+            {...register("password")}
+            error={!!errors.password}
+            helperText={errors.password?.message}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton onClick={togglePasswordVisibility} edge="end">
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+
+          {/* Submit Button */}
+          <Button type="submit" variant="contained" color="primary" sx={{ width: "100px" }}>
+            Login
+          </Button>
         </Box>
       </form>
     </Container>
